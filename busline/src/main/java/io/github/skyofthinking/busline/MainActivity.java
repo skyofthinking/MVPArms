f253a6bb344e305f3a8643341033216256b2ba78
kestrel <starsiday@hotmail.com>
2017-05-08 17:19:42 +0800

    add toolbar

--- busline/src/main/java/io/github/skyofthinking/busline/MainActivity.java
+++ busline/src/main/java/io/github/skyofthinking/busline/MainActivity.java
@@ -1,7 +1,9 @@
 package io.github.skyofthinking.busline;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        // toolbar.setLogo(R.drawable.ic_launcher);
+        mToolbar.setTitle("Rocko");// 标题的文字需在setSupportActionBar之前，不然会无效
+        mToolbar.setSubtitle("副标题");
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
